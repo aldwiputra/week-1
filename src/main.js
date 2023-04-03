ce58bb28a6cae1fa62e30a85b8589543289ca1fc
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-04-03 13:04:49 +0700

    test

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <h1 class="text-orange-700">Hello</h1>
+`;
---
+++ vite.config.js
@@ -0,0 +1,11 @@
+/** @type {import('vite').UserConfig} */
+export default {
+  pages: {
+    '/register': {
+      entry: 'src/pages/register/index.html',
+    },
+    '/login': {
+      entry: 'src/pages/login/index.html',
+    },
+  },
+};
